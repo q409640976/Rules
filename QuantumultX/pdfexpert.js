f794de0442682d9fdb769671e8da21c0598ee219
Apocalypsor <Apocalypsor@users.noreply.github.com>
2019-10-27 21:20:34 +0800

    Update pdfexpert.js

--- QuantumultX/pdfexpert.js
+++ QuantumultX/pdfexpert.js
@@ -1,5 +1,12 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-obj.subscriptionExpirationDate = "13:15 03/11/2099"
-body = JSON.stringify(obj);
-$done(body);
+let obj = JSON.parse($response.body);
+obj = {
+  "originalTransactionId": "330000538589189",
+  "subscriptionState": "trial",
+  "isInGracePeriod": false,
+  "subscriptionExpirationDate": "13:59 03/11/2999",
+  "subscriptionAutoRenewStatus": "autoRenewOff",
+  "isEligibleForIntroPeriod": false,
+  "isPDFExpert6User": true,
+  "subscriptionReceiptId": "1553358285000"
+};
+$done({body: JSON.stringify(obj)});
